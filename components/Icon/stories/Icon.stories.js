61c366609a3aaed71be27d9545297e86512997d4
hileix <304192604@qq.com>
2019-05-05 17:48:27 +0800

    feat:添加复制 icon 组件功能

--- components/Icon/stories/Icon.stories.js
+++ components/Icon/stories/Icon.stories.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import ClipboardJS from 'clipboard';
+import { message } from 'antd';
+import * as icons from 'ic-icons';
+import AntdIcon from '@ant-design/icons-react';
 import Icon from '../';
-
 import docs from './docs.md';
-
 import '../style/index.less';
+import './style.less';
 
-const SingeIcon = ({ onCopy, type, size }) => {
+const iconKeys = Object.keys(icons);
+iconKeys.forEach(key => {
+  AntdIcon.add(icons[key]);
+});
+
+const SingeIcon = ({ type, size }) => {
   return (
-    <div onClick={() => onCopy(`<Icon type={${type}} />`)}>
+    <div data-clipboard-text={`<Icon type="${type}" />`} className={`single-icon icon__${type}`}>
       <Icon type={type} size={size} />
       <span>{type}</span>
     </div>
@@ -16,14 +24,29 @@ const SingeIcon = ({ onCopy, type, size }) => {
 };
 
 class IconTest extends React.Component {
-  handleCopy = () => {
-    console.log('copied');
+  iconsType = [];
+
+  componentDidMount = () => {
+    this.iconsType = iconKeys.map(iconKey => {
+      const index = iconKey.indexOf('Out');
+      return iconKey.substring(0, index).toLocaleLowerCase();
+    });
+
+    this.iconsType.forEach(type => {
+      this[type] = new ClipboardJS(`.icon__${type}`);
+      this[type].on('success', function(e) {
+        message.success('复制成功');
+      });
+    });
+    this.forceUpdate();
   };
 
   render() {
     return (
-      <div>
-        <SingeIcon onCopy={this.handleCopy} type="search" size={36} />
+      <div className="icon__test">
+        {this.iconsType.map(type => (
+          <SingeIcon key={type} type={type} size={36} />
+        ))}
       </div>
     );
   }
