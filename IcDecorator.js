ec21cd45c0f627f9473c5796c7638675f9c1a88f
hileix <304192604@qq.com>
2019-05-08 10:06:23 +0800

    feat:IcDecorator 组件添加 style

--- IcDecorator.js
+++ IcDecorator.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const IcDecorator = ({ children, info }) => {
+const IcDecorator = ({ children, info, style }) => {
   return (
-    <div>
-      <div
-        className="ic-demo-wrapper"
-        style={{ display: 'inline-block', padding: 24, border: '1px solid #eee', margin: 16, borderRadius: 8 }}
-      >
-        <div className="ic-demo-wrapper__content" style={{ margin: 16 }}>
-          {children}
-        </div>
-        <div className="ic-demo-wrapper__code" style={{ borderTop: '1px solid #eee', paddingTop: 24 }}>
-          <div>{info}</div>
-        </div>
+    <div
+      className="ic-demo-wrapper"
+      style={{ display: 'inline-block', padding: 24, border: '1px solid #eee', margin: 16, borderRadius: 8 }}
+    >
+      <div className="ic-demo-wrapper__content" style={{ marginBottom: 16, ...style }}>
+        {children}
+      </div>
+      <div className="ic-demo-wrapper__info" style={{ borderTop: '1px solid #eee', paddingTop: 24 }}>
+        <div>{info}</div>
       </div>
     </div>
   );
