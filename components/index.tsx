ad8beea33e304b93ded6639a926ef9dcb4278257
hileix <304192604@qq.com>
2019-05-13 17:11:22 +0800

    chore: export components in index.tsx

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,19 @@
+export { default as Button } from './Button';
+
 export { default as Calenaar } from './Calendar';
+
+export { default as Checkbox } from './Checkbox';
+
+export { default as EventBar } from './EventBar';
+
+export { default as Icon } from './Icon';
 
 export { default as Input } from './Input';
 
+export { default as Modal } from './Modal';
+
 export { default as Search } from './Search';
+
+export { default as Select } from './Select';
+
+export { default as Switch } from './Switch';
