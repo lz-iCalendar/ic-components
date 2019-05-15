539e93ed13ec9de2fbe058281535038142877cd7
hileix <304192604@qq.com>
2019-05-15 14:53:21 +0800

    feat:export TextArea and Popover

--- components/index.tsx
+++ components/index.tsx
@@ -17,3 +17,7 @@ export { default as Search } from './Search';
 export { default as Select } from './Select';
 
 export { default as Switch } from './Switch';
+
+export { default as TextArea } from './TextArea';
+
+export { default as Popover } from './Popover';
